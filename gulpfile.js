2789a33e939d8b6d24e6a46a57b4644c436071ab
MIROSH2X20 <59619932+Ssklvsk@users.noreply.github.com>
2021-04-09 18:14:09 +0300

    added popup

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task('script', function(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js'
     
 
@@ -37,9 +38,11 @@ gulp.task('script', function(){
 
 gulp.task('style', function(){
   return gulp.src([
+    'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
-    'node_modules/rateyo/src/jquery.rateyo.css'
+    'node_modules/rateyo/src/jquery.rateyo.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
   ])
   .pipe(concat('libs.min.css'))
   .pipe(cssmin())
